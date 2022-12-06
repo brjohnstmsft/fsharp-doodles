62df802f1892453348237c7b36747daf945a96b5
Bruce Johnston <brjohnst@microsoft.com>
2022-12-06 14:44:18 -0800

    Advent of Code 2022, Day 6: Elf Tuning Trouble

---
+++ AdventOfCode2022/ElfTuningTrouble/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let findUniqueSequence desiredSize (buffer: char[]) =
+    let mutable start = 0
+    let mutable end' = 0
+    let mutable found = false
+    while end' < buffer.Length && not found do
+        let newChar = buffer[end']
+        let length = end' - start + 1
+        let dupIndex = Array.IndexOf(buffer, newChar, start, length - 1)
+        if dupIndex <> -1 then
+            start <- dupIndex + 1
+        else if length = desiredSize then
+            found <- true
+
+        end' <- end' + 1
+
+    if not found then failwith "Invalid buffer"
+    else end'
+
+let findHeader = findUniqueSequence 4
+let findMessage = findUniqueSequence 14
+
+let buffer = File.ReadAllText("input.txt").ToCharArray()
+
+printfn $"Found header after reading {findHeader buffer} characters."
+
+printfn $"Found message after reading {findMessage buffer} characters."
